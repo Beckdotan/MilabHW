af1c0e103429131901837ce78f5f5a8209a12711
Beckdotan <dotan.beck@gmail.com>
2021-12-10 12:13:48 +0200

    Working version

--- Hw4/mainPipe.js
+++ Hw4/mainPipe.js
@@ -6,30 +6,84 @@ const fs = require('fs')
 
 
 app.get('/',(req,res) => {
-	res.send('Welcome To my Server');
+	res.send('Welcome To my Server - You will probeble want to got to:\nhttp://localhost:8080/files');
 });
 
-/////////////////////////////////////////////////////
 
-app.get('/DontBeScared',(req,res) => {
+////////  - \files -    ///////// 
+
+app.get('/files',(req,res) => {
+	var text = 'Plese add to the adress line on of the next options:\n/scary\nor\n /nice\nor\n /awesome\n In addition, please add in the add of the aress line: ?name=YOUR NAME'
+	res.send(text);
+});
+
+
+
+
+////////////////// -Scary - ///////////////////////////////////
+
+app.get('/files/Scary',(req,res) => {
 	
 	let name = req.query.name || "<unknown>"
 
+	var text = 'You Are Too Scread To Give Me Your Name HA?\n lets try again, add to aress line ?name=<YOUR NAME>'
+
 	if (name == "<unknown>"){
-		res.send("You Are Too Scread To Give Me Your Name HA? - lets try again, add to line ?name=<YOUR NAME>");
+		res.send(text);
 
 	}else{
 
-		var data = fs.readFileSync(__dirname + '/start.txt');
+		var data = fs.readFileSync(__dirname + '/scary.txt');
 		console.log(data);
-  		var readStream	= fs.createReadStream(__dirname + '/start.txt');
-  		var writeStream = res.send(name + ' ' + data)
+  		var readStream	= fs.createReadStream(__dirname + '/Scary.txt');
+  		var writeStream = res.send(name + data)
   		readStream.pipe(writeStream);
 	}
 
+});
 
+////////////////// -Nice - ///////////////////////////////////
+
+app.get('/files/nice',(req,res) => {
 	
+	let name = req.query.name || "<unknown>"
 
+	var text = 'Hi, what is you name? \n Please add to adress line ?name=<YOUR NAME>'
+
+	if (name == "<unknown>"){
+		res.send(text);
+
+	}else{
+
+		var data = fs.readFileSync(__dirname + '/nice.txt');
+		console.log(data);
+  		var readStream	= fs.createReadStream(__dirname + '/nice.txt');
+  		var writeStream = res.send(data + name)
+  		readStream.pipe(writeStream);
+	}
+
+});
+
+
+////////////////// -awesome - ///////////////////////////////////
+
+app.get('/files/awesome',(req,res) => {
+	
+	let name = req.query.name || "<unknown>"
+
+	var text = 'YOU ARE AWESOME! \nBut please add to adress line ?name=<YOUR NAME>'
+
+	if (name == "<unknown>"){
+		res.send(text);
+
+	}else{
+
+		var data = fs.readFileSync(__dirname + '/awesome.txt');
+		console.log(data);
+  		var readStream	= fs.createReadStream(__dirname + '/awesome.txt');
+  		var writeStream = res.send(name + data)
+  		readStream.pipe(writeStream);
+	}
 
 });
 
