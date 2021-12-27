c03ef74d170a843f9d0775cfc0ef7823afdfbd59
Beckdotan <dotan.beck@gmail.com>
2021-12-27 18:10:32 +0200

    withe better server ad fetcher function

--- StockApp/app/src/main/java/com/example/stockapp/MainActivity.java
+++ StockApp/app/src/main/java/com/example/stockapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.stockapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -34,8 +35,29 @@ public class MainActivity extends AppCompatActivity {
                 stockName = (String) userText.getText().toString();
                 //log.i("Main Activity Got Stock Name", stockName);
                 resultTextView.setText(stockName);
+                fetchStockPrice(view);
             }
         });
 
     }
+
+
+    public void fetchStockPrice(final View view) {
+        final StockFetcher fetcher = new StockFetcher(view.getContext());
+        final ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Fetching Stock...");
+        progressDialog.show();
+
+        fetcher.dispatchRequest(new StockFetcher.StockResponseListener() {
+            @Override
+            public void onResponse(StockFetcher.StockResponse response) {
+                progressDialog.hide();
+
+                if (response.isError) {
+                    Toast.makeText(view.getContext(), "Error while fetching Stock", Toast.LENGTH_LONG);
+                    return;
+                }
+                //didnt changed it yet.
+                ((TextView)MainActivity.this.findViewById(R.id.location)).setText(response.location);
+                ((TextView)MainActivity.this.findViewById(R.id.temprature)).setText(String.valueOf(response.temp));
 }
--- StockApp/index.js
+++ StockApp/index.js
@@ -1,17 +1,40 @@
 const express = require('express');
+let app = express();
 const request = require('request');
 const PORT = 8080;
-let app = express();
+
 APIKEY = 'BYHKP7FYWBWRVJN3';
 
 
 
 app.get('/stock', (req, res, next) => {
+	/*
 	var text = '<html><body>hey there<br>it was a good lecture</body><html>'
 	res.send(text);
+	*/
 
 
 
+ 	let stockSymbol = req.params.stock_name;
+	let url = `https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${stockSymbol}&apikey=${APIKEY}`
+
+
+	request(url, function (err, response, body) {
+        if(err){
+            console.log('error:', error);
+            return res.error(err);
+          } else {
+            let stockData = JSON.parse(body)
+			
+			//Testings
+			const stockPrice = parseFloat(body['Global Quote']['05. price']);
+		    let message = `The price of ${stockName} is ${stockPrice}`
+            console.log(message);
+			
+            //return res.json(stockData);
+          }
+    });
+ 
 
 });
 
